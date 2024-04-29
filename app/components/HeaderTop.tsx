7c43f4c6a184b59dfe218cb015b225ce53ed821d
Ovigwe M. Akpokighe <m.ovigwe@yahoo.com>
2024-04-29 09:42:08 +0100

    NextJs Tailwind Shopping App

---
+++ app/components/HeaderTop.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
+
+const HeaderTop = () => {
+  return (
+    <div className="border-b border-gray-300 hidden sm:block">
+      <div className="container py-4">
+        <div className="flex justify-between items-center">
+            <div className="hidden lg:flex gap-4">
+                <div className="header_top__icon_wrapper">
+                <BsFacebook />
+                </div>
+                <div className="header_top__icon_wrapper">
+                <BsTwitter />
+                </div>
+                <div className="header_top__icon_wrapper">
+                <BsInstagram />
+                </div>
+                <div className="header_top__icon_wrapper">
+                <BsLinkedin />
+                </div>
+              </div>
+
+            <div className="text-gray-500 text-[12px]">
+             <b> FREE SHIPPING</b> THIS WEEK ORDER OVER - 55$
+              </div>
+
+            <div className="flex gap-4">
+              <select 
+              className="text-gray-500 text-[12px] w-[70px]"
+                name="currency"
+                id="currency"
+                >
+                  <option value="USD" $>USD $</option>
+                  <option value="USD" $>EUR $</option>
+                  <option value="USD" $>INR $</option>
+
+              </select>
+              <select 
+              className="text-gray-500 text-[12px] w-[80px]"
+                name="language"
+                id="language"
+                >
+                  <option value="USD" $>English</option>
+                  <option value="USD" $>French</option>
+
+              </select>
+
+            </div>
+
+
+
+
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default HeaderTop
